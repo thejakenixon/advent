9283fb50faa0fa7d980c1073c35b43e715acfb52
thejakenixon <688940+thejakenixon@users.noreply.github.com>
2020-12-03 18:45:38 -0700

    Small updates to day 3 code

--- 20/03/p1.py
+++ 20/03/p1.py
@@ -4,22 +4,30 @@ dataArray = data.read().splitlines()
 height = len(dataArray)
 width = len(dataArray[0])
 down = 1
-right = 3
+right = 1
 treecount = 0
-
 iteration = 0
+
+xindex = 0
+yindex = 0
 
 for x in range(int(round(height/down))):
     iteration += 1
-    xindex = right*(iteration)
+    xindex += right 
     yindex = down*(iteration)
 
-    print(xindex)
-    print(yindex,'y')
-    if xindex >= width: xindex -= width
+    print(xindex,'x')
+    #print(yindex,'y')
+    if xindex >= width:
+        xindex -= 30
+        print('aaaaaa')
 
-    #print(dataArray[yindex][xindex])
+    #print(dataArray[yindex][xindex],'here')
     if(dataArray[yindex][xindex])=='#':
         treecount +=1
-    print(iteration)
-    print(treecount)
+    #print(iteration,'it')
+    print(width,'width')
+    #print(treecount,'trees')
+#    print(type(xindex))
+#    print(type(width))
+    input('')
